75a603c194638916eb44d59bce4db95f590da17c
jamesncox <jamesncox@yahoo.com>
2021-06-07 18:03:38 -0500

    Add comments for additions to UI and functions to handle toggling text-size

--- src/App.js
+++ src/App.js
@@ -1,33 +1,47 @@
-import './App.css';
+import "./App.css";
+// refactor to the descructed import {useState} from "react" because it's more familiar to me
 import React from "react";
 
 function App() {
-  const [textInput, setTextInput] = React.useState('Here is some example text.');
-  const [textOutput, setTextOutput] = React.useState('');
+  // refactor calling useState without referencing React here
+  const [textInput, setTextInput] = React.useState(
+    "Here is some example text."
+  );
+  const [textOutput, setTextOutput] = React.useState("");
 
-  const handleChange = event => {
+  const handleChange = (event) => {
     setTextInput(event.target.value);
   };
 
-  const handleSubmit = event => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    setTextOutput('Your formatted text will go here!')
+    setTextOutput("Your formatted text will go here!");
   };
+
+  // Create two functions, handleUpperCase and handleLowerCase that are
+  // wired up to the two buttons for toggling textOutput size
+
+  // If I create a button for clearing textInput and textOutput,
+  // create a function called clearTextInputAndOutput that resets the state of each.
 
   return (
     <div className="App">
       <header>
         <h1>Career Lab | Take-Home Assignment</h1>
       </header>
+      {/* Does this form need some more accessibility considerations? */}
+      {/* The form is currently labelled implicitly and I think explicitly is better. */}
       <form onSubmit={handleSubmit}>
         <label>
-          <textarea onChange={handleChange} value={textInput}/>
+          <textarea onChange={handleChange} value={textInput} />
         </label>
-        <input type="submit" value="Submit"/>
+        <input type="submit" value="Submit" />
       </form>
-      <div id="result">
-        {textOutput}
-      </div>
+      {/* Is a <div> here for textOutput without a <p> tag or some other element as accessible and semantic? */}
+      <div id="result">{textOutput}</div>
+      {/* Create a UI to select/switch between two text size modes: */}
+      {/* Create two buttons, one that handles upper-casing textOutput and the other that handles lower-casing textOutput */}
+      {/* Could be nice to have a button that also that clears textInput and textOutput*/}
     </div>
   );
 }
