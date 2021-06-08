2c9be3512f0c4a27d1ca001ab1a4ff2e2cc70ea0
jamesncox <jamesncox@yahoo.com>
2021-06-08 10:45:40 -0500

    Remove redundant code comments that the code explains for itself

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import "./App.css";
-// refactor to the descructed import {useState} from "react" because it's more familiar to me
 import React, { useState } from "react";
 
 function App() {
-  // refactor calling useState without referencing React here
   const [textInput, setTextInput] = useState("");
   const [textOutput, setTextOutput] = useState("");
 
@@ -17,20 +15,14 @@ function App() {
     setTextOutput(textInput ? textInput : "Your formatted text will go here!");
   };
 
-  // Create two functions, handleUpperCase and handleLowerCase that are
-  // wired up to the two buttons for toggling textOutput size
   const handleUpperCase = () => {
-    // update the state of textOutput with .toUpperCase()
     setTextOutput(textOutput.toUpperCase());
   };
 
   const handleLowerCase = () => {
-    // update the state of textOutput with .toLowerCase()
     setTextOutput(textOutput.toLowerCase());
   };
 
-  // If I create a button for clearing textInput and textOutput,
-  // create a function called clearTextInputAndOutput that resets the state of each.
   const clearInputAndOutputText = () => {
     setTextInput("");
     setTextOutput("");
@@ -41,8 +33,6 @@ function App() {
       <header>
         <h1>Career Lab | Take-Home Assignment</h1>
       </header>
-      {/* Does this form need some more accessibility considerations? */}
-      {/* The form is currently labelled implicitly and I think explicitly is better. */}
       <form onSubmit={handleSubmit}>
         <label aria-label="text input">
           <textarea
@@ -53,13 +43,9 @@ function App() {
         </label>
         <input type="submit" value="Submit" />
       </form>
-      {/* Is a <div> here for textOutput without a <p> tag or some other element as accessible and semantic? */}
       <div id="result">
         <p aria-label="text output">{textOutput}</p>
       </div>
-      {/* Create a UI to select/switch between two text size modes: */}
-      {/* Create two buttons, one that handles upper-casing textOutput and the other that handles lower-casing textOutput */}
-      {/* Could be nice to have a button that also that clears textInput and textOutput*/}
       <button onClick={handleUpperCase}>UPPERCASE</button>
       <button onClick={handleLowerCase}>lowercase</button>
       <button onClick={clearInputAndOutputText}>Clear Text</button>
